2a82ca705110cfb95a59a3a793c4396cdfbf1041
Tyler Dooling <tdooling@decisiv.net>
2013-08-12 14:47:49 -0500

    Adds live weather stations.

--- lib/noaa_client/client.rb
+++ lib/noaa_client/client.rb
@@ -1,9 +1,14 @@
 require_relative 'services/forecast_by_day'
+require_relative 'services/weather_stations'
 
 module NoaaClient
   class Client
     def forecast_by_day(lat, lon, options = {})
       parse_service(options).fetch(lat, lon, options)
+    end
+
+    def weather_stations(options = {})
+      Services::WeatherStations.new(options).fetch(options)
     end
 
     private
---
+++ lib/noaa_client/services/weather_stations.rb
@@ -0,0 +1,33 @@
+require_relative 'rest_service'
+require_relative '../xml_parser_factory'
+require_relative '../responses/stations'
+
+module NoaaClient
+  module Services
+    class WeatherStations
+      include RestService
+
+      def initialize(options = {})
+        @options = options
+      end
+
+      def fetch(options = {})
+        rest_service.object_from_response(:get,
+                                          'http://w1.weather.gov/xml/current_obs/index.xml',
+                                          response_class: response_class)
+      end
+
+      private
+
+      attr_reader :options
+
+      def rest_service
+        options.fetch(:rest_service, self)
+      end
+
+      def response_class
+        options.fetch(:response_class, Responses::Stations)
+      end
+    end
+  end
+end
