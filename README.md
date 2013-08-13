445ca6a41c26b26bd5eb10b598fa60db6629ebf0
Tyler Dooling <tdooling@decisiv.net>
2013-08-13 12:11:30 -0500

    Add documentation.

--- README.md
+++ README.md
@@ -16,22 +16,40 @@ Or install it yourself as:
 
     $ gem install noaa_client
 
+
+## Notes
+
+### Weather Station Caching
+
+It is important to cache a copy of the available stations for use here, as the xml stations response is quite large noaa does not appreciate repeated calls.
+
+```ruby
+# cache a copy of stations and store in memory, file, etc.
+stations = client.nearest_weather_station(coordinate.latitude, coordinate.longitude)
+
+# current_observations
+client.current_observations(some_lat, some_lon, stations: stations)
+
+# nearest_weather_station
+client.nearest_weather_station(some_lat, some_lon, stations: stations)
+```
+
 ## Usage
 
 ```ruby
 client = NoaaClient.build_client
+
+# all weather stations
+client.weather_stations
+
+# nearest_weather_station
+client.nearest_weather_station(coordinate.latitude, coordinate.longitude)
 
 # convert zip to lat/lon
 coordinate = client.zip_code_to_lat_lon(90210)
 
 # 7 day forecast
 client.forecast_by_day(coordinate.latitude, coordinate.longitude)
-
-# nearest_weather_station
-client.nearest_weather_station(coordinate.latitude, coordinate.longitude)
-
-# all weather stations
-client.nearest_weather_station(coordinate.latitude, coordinate.longitude)
 
 # current observations
 client.current_observations(coordinate.latitude, coordinate.longitude)
--- lib/noaa_client/client.rb
+++ lib/noaa_client/client.rb
@@ -5,25 +5,50 @@ require_relative 'services/find_nearest_station'
 require_relative 'services/zip_code_to_lat_lon'
 
 module NoaaClient
+  # Provides entry point for interacting with the noaa api via multiple services.
   class Client
+    # Fetches a daily forecast for a location. Default is 7 days.
+    # @param lat [Float] latitude
+    # @param lon [Float] longitude
+    # @return [Responses::Forecast] a list of daily forecasts.
     def forecast_by_day(lat, lon, options = {})
       parse_service(options).fetch(lat, lon, options)
     end
 
+    # Retrieves a list of weather stations from noaa.
+    # @return [Responses::Stations] a list of weather stations.
     def weather_stations(options = {})
       Services::WeatherStations.new(options).fetch
     end
 
+    # Retrieves the current weather observations for a location.
+    # @note It is important to cache a copy of the available stations for use here, as the xml stations response is quite large noaa does not appreciate repeated calls.
+    # @param lat [Float] latitude
+    # @param lon [Float] longitude
+    # @param [Hash] options
+    # @option options [Responses::Stations] stations A cached stations response to prevent having to query noaa for the list.
+    # @return [Responses::CurrentObservation]
     def current_observations(lat, lon, options = {})
       station = options.fetch(:station, nearest_weather_station(lat, lon, options))
       Services::CurrentObservations.new(options).fetch(station)
     end
 
+    # Retrieves the weather station nearest to the location.
+    # @note It is important to cache a copy of the available stations for use here, as the xml stations response is quite large noaa does not appreciate repeated calls.
+    # @param lat [Float] latitude
+    # @param lon [Float] longitude
+    # @param [Hash] options
+    # @option options [Responses::Stations] stations A cached stations response to prevent having to query noaa for the list.
+    # @return [Responses::Station]
     def nearest_weather_station(lat, lon, options = {})
       stations = options.fetch(:stations, weather_stations)
       Services::FindNearestStation.find(lat, lon, stations)
     end
 
+    # Converts a zip code to a latitude and longitude.
+    # @param lat [Float] latitude
+    # @param lon [Float] longitude
+    # @return [Responses::LatLonList]
     def zip_code_to_lat_lon(zip, options = {})
       Services::ZipCodeToLatLon.new(options).convert(zip)
     end
