246d800e5822f15d5045ae9f4abe989c83ef4efd
Tyler Dooling <tdooling@decisiv.net>
2014-02-21 12:10:22 -0600

    Remove duplicate logic.

--- lib/noaa_client/responses/station.rb
+++ lib/noaa_client/responses/station.rb
@@ -23,22 +23,6 @@ module NoaaClient
                        "http://w1.weather.gov#{path}"
                      end
       end
-
-      private
-
-      attr_reader :source
-
-      def method_missing(method_name, *arguments, &block)
-        if tag = source.css(method_name.to_s)
-          tag.text
-        else
-          super
-        end
-      end
-
-      def respond_to_missing?(method_name, include_private = false)
-        source.css(method_name.to_s) || super
-      end
     end
   end
 end
