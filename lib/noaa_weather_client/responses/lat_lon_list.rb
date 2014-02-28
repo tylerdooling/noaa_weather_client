a969b53025ee597b756d38c7b51db982d7178fde
Tyler Dooling <tdooling@decisiv.net>
2014-02-27 21:46:04 -0600

    Add schema validation to lat lon list response.

--- lib/noaa_weather_client/responses/lat_lon_list.rb
+++ lib/noaa_weather_client/responses/lat_lon_list.rb
@@ -1,13 +1,16 @@
 require_relative '../xml_parser_factory'
 require_relative 'reactive_xml_response'
+require_relative 'validatable_xml_response'
 
 module NoaaWeatherClient
   module Responses
     class LatLonList
       include ReactiveXmlResponse
+      include ValidatableXmlResponse
 
       def initialize(response)
         @source = XmlParserFactory.build_parser.parse(response)
+        validate! @source, :dwml
       end
 
       def latitude
